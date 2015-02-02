97b95801d5d228a5634a8cca3cfcc98154de75fc
garciadanny <dannygarcia.me@gmail.com>
2015-02-02 15:48:20 -0700

    add helper methods and test emails for specs

---
+++ spec/helpers/original_email_helper.rb
@@ -0,0 +1,16 @@
+module OriginalEmailHelper
+
+  def original_email
+    location = file_location( 'test_email.txt' )
+    File.open(location, 'r') { |file| file.read }
+  end
+
+  def header_string
+    location = file_location( 'test_header.txt' )
+    File.open(location, 'r') { |file| file.read }
+  end
+
+  def file_location file_name
+    File.join(File.dirname(__FILE__), "../test_emails/#{file_name}")
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,13 @@
-Dir[File.join(File.dirname(__FILE__), '../lib', '**.rb')].each do |f|
+Dir[File.join(File.dirname(__FILE__), '../lib', '**/*.rb')].each do |f|
   require f
 end
 
 require 'rspec'
+require 'pry'
+require 'helpers/original_email_helper'
 
 RSpec.configure do |config|
   config.order = 'random'
+  config.color = true
+  config.include OriginalEmailHelper
 end
