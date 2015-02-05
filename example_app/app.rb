7d13f53142e587a722ad0095d475c0f7c012b010
garciadanny <dannygarcia.me@gmail.com>
2015-02-05 16:57:53 -0700

    add view layout and partial that displays email list

--- example_app/app.rb
+++ example_app/app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
+require_relative 'helpers/utils'
+require_relative 'helpers/view_helpers'
+require_relative '../lib/email'
 
 class MyApp < Sinatra::Application
+
+  helpers Utils, ViewHelpers
+
+  before do
+    @emails = email_list
+  end
 
   get '/' do
     erb :index
